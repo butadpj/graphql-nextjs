10ec33eaf5264e49d2154176cd05ac8426924410
butadpj <butadpj@gmail.com>
2025-01-17 01:59:10 +0800

    prerender / page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { countryListsQuery } from "./lib/queries";
 import { Suspense } from "react";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 export const revalidate = 60;
 
 export default async function Home() {
