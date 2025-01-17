f9762b966fa13392388c041695f3336bf4a438cb
butadpj <butadpj@gmail.com>
2025-01-17 22:10:43 +0800

    add caching on latest country

--- app/components/latest-country.tsx
+++ app/components/latest-country.tsx
@@ -3,17 +3,10 @@ import PreloadQuery from "../lib/preload-query";
 import { latestCountryQuery } from "../lib/queries";
 
 export default async function LatestCountry() {
-  const client = new GraphQLClient(
-    `${process.env.NEXT_PUBLIC_HOST}/graphql`,
-    {
-      cache: "no-store",
-    }
-  );
-  const data = await new Promise((resolve) => {
-    // setTimeout(() => {
-    resolve(client.request(latestCountryQuery));
-    // }, 3000);
+  const client = new GraphQLClient(`${process.env.NEXT_PUBLIC_HOST}/graphql`, {
+    // cache: "no-store",
   });
+  const data = await client.request(latestCountryQuery);
 
   return (
     <h1 style={{ textAlign: "center" }}>
